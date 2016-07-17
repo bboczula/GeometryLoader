9c90eb1907da0492c743c1ade9e4d97d1b5ef1c6
bboczula <bb.bartosh@gmail.com>
2016-07-17 11:00:55 +0200

    Add files via upload
    Initial ib-geometryloader-ref

---
+++ ib-geometryloader/ib-geometryloader-ref/ib-geometryloader-ref.cpp
@@ -0,0 +1,24 @@
+// ib-geometryloader-ref.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "IGeometryLoader.h"
+
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	IGeometryLoader* geometryLoader = CreateGeometryLoader();
+
+	geometryLoader->loadFromFile("..\\Debug\\geometry\\cube.obj");
+	for (int i = 0; i < geometryLoader->getNumOfVertices(); i++)
+	{
+		FLOAT3 vertex = geometryLoader->vertexAt(i);
+		std::cout << "v " << vertex << std::endl;
+	}
+	delete geometryLoader;
+
+	getchar();
+	return 0;
+}
